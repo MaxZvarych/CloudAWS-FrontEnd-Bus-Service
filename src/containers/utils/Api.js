32951c29f65b863fc10edf8cb98166b169f759e1
Maksym-Fedir Zvarych <maksym.zvarych@intellias.com>
2021-11-04 18:15:50 +0200

    Edited Api.js

--- src/containers/utils/Api.js
+++ src/containers/utils/Api.js
@@ -16,12 +16,7 @@ export const getAllBuses = async () => {
 export const postBus = async (body) => {
   // const json = JSON.stringify(body);
   try {
-    let responseData = await axios.post(baseURL, body, {
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-    });
+    let responseData = await axios.post(baseURL, body);
     console.log(responseData);
     return responseData.data;
   } catch {
